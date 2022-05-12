1bfaae04e353f065660a7ee65601a3cbbbd4de41
Kiran kumari Giri <59924309+kiran22985@users.noreply.github.com>
2022-05-12 20:37:01 +0545

    OTP UI completed

--- lib/screen/otp1.dart
+++ lib/screen/otp1.dart
@@ -11,15 +11,56 @@ class _OTP1State extends State<OTP1> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(child: Column(
-        children:const  [
-          Image(image: AssetImage('assets/images/')),
-          SizedBox(height: 30,),
-          Text('OTP Verification',style: TextStyle(
-            fontWeight: FontWeight.bold,
-            fontSize: 30,
-          ),)
-        ],
+      body: SafeArea(child: Center(
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment:MainAxisAlignment.center,
+            children:  [
+              const Image(image: AssetImage('assets/images/otp.png'),width: 350,height: 230,),
+              const SizedBox(height: 30,),
+              const Text('OTP Verification',style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 25,
+              ),),
+              const SizedBox(height: 20,),
+              const Text('We will send you 4 digits OTP code on this email ',textAlign: TextAlign.center,),
+              //const SizedBox(height: 20,),
+              Container(
+                padding: const EdgeInsets.fromLTRB(40, 20, 40, 0),
+                child: Material(
+                  elevation: 10.0,
+                  shadowColor: Colors.white,
+                  child: TextFormField(
+                    keyboardType: TextInputType.emailAddress, 
+                    decoration: InputDecoration(
+                      hintText: 'anushkagiri2298@gmail.com',
+                      labelText: 'Email',
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                        
+                      ),
+                        
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(height: 30,),
+              ElevatedButton(onPressed: (){
+                    Navigator.pushNamed(context, '/otp2');
+                  },
+                  
+                   child: const Text('Get OTP',textScaleFactor: 1.2,),style: ElevatedButton.styleFrom(
+                    primary: Colors.blue,
+                    minimumSize: const Size(150,40),
+                    elevation: 10,
+                    shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20)),
+                    
+                  ),
+                  )
+            ],
+          ),
+        ),
       )),
       
     );
