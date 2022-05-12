430304ba945555a61e288087b143adf4ef7eaf55
Kiran kumari Giri <59924309+kiran22985@users.noreply.github.com>
2022-05-12 12:06:18 +0545

    otp1 completed

---
+++ lib/screen/otp1.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class OTP1 extends StatefulWidget {
+  const OTP1({ Key? key }) : super(key: key);
+
+  @override
+  State<OTP1> createState() => _OTP1State();
+}
+
+class _OTP1State extends State<OTP1> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+      
+    );
+  }
+}
